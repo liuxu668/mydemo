38e0f8939088ca718e43c6cc4c672140ea3cfe1e
liuxu668 <2458554813@qq.com>
2019-09-26 23:09:11 +0800

    更新

--- src/main/java/org/java/mydemo/MydemoApplication.java
+++ src/main/java/org/java/mydemo/MydemoApplication.java
@@ -1,7 +1,9 @@
 package org.java.mydemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class MydemoApplication {
--- src/main/java/org/java/mydemo/controller/FirstController.java
+++ src/main/java/org/java/mydemo/controller/FirstController.java
@@ -1,12 +1,17 @@
 package org.java.mydemo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Api(description = "第一个测试接口")
 @Controller
 public class FirstController {
 
-    @RequestMapping("/first")
+    @ApiOperation(value = "第一个测试方法")
+    @RequestMapping(value = "/first",method = {RequestMethod.GET,RequestMethod.POST})
     public String first(){
         return "index";
     }
